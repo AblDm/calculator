3b925e7708b2b67d26de8f8d5096b922db5ce4d0
Dmabl <sdfghj@dg.jnb>
2022-12-05 01:45:20 +0300

    rework1

--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -1,11 +1,10 @@
 package com.example.calculator;
 
-import com.example.calculator.CalculatorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/calculator")
+@RequestMapping(" ")
 public class CalculatorController {
 
     private final CalculatorService calculatorService;
@@ -23,25 +22,25 @@ public class CalculatorController {
     public String hello() {return "Hello, username!"; }
 
     @GetMapping("/plus")
-    public String plus (@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+    public String plus (@RequestParam("num1") Integer num1, @RequestParam("num2") Integer num2){
         int result = calculatorService.plus (num1, num2);
         return String.format ("%d + %d = %d", num1, num2, result);
     }
 
     @GetMapping("/minus")
-    public String minus (@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+    public String minus (@RequestParam("num1") Integer num1, @RequestParam("num2") Integer num2){
         int result = calculatorService.minus (num1, num2);
         return String.format ("%d - %d = %d", num1, num2, result);
     }
 
     @GetMapping("/multiple")
-    public String multiple (@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+    public String multiple (@RequestParam("num1") Integer num1, @RequestParam("num2") Integer num2){
         int result = calculatorService.multiple (num1, num2);
         return String.format ("%d * %d = %d", num1, num2, result);
     }
 
     @GetMapping("/divide")
-    public String divide (@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+    public String divide (@RequestParam("num1") Integer num1, @RequestParam("num2") Integer num2){
         int result = calculatorService.divide (num1, num2);
         return String.format ("%d / %d = %d", num1, num2, result);
     }
--- src/main/java/com/example/calculator/CalculatorService.java
+++ src/main/java/com/example/calculator/CalculatorService.java
@@ -4,25 +4,34 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
+int num1;
 
-
-    public int divide(int num1, int num2) {
+    public Integer divide(Integer num1, Integer num2) {
         if (num2==0){
             throw new IllegalNumberException ("Нельзя делить на ноль");
         }
         return num1/num2;
     }
 
-    public int plus(int num1, int num2) {
+    public int plus(Integer num1, Integer num2) {
+        if (num2==null ||num1 == null){
+            throw new IllegalNumberException ("Необходимо ввести оба аргумента для работы калькулятора");
+        }
         return num1+num2;
     }
 
 
-    public int minus(int num1, int num2) {
+    public int minus(Integer num1, Integer num2) {
+        if (num2==null ||num1 == null){
+            throw new IllegalNumberException ("Необходимо ввести оба аргумента для работы калькулятора");
+        }
         return num1-num2;
     }
 
-    public int multiple(int num1, int num2) {
+    public int multiple(Integer num1, Integer num2) {
+        if (num2==null ||num1 == null){
+            throw new IllegalNumberException ("Необходимо ввести оба аргумента для работы калькулятора");
+        }
         return num1*num2;
     }
 }
